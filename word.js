814f14b8d0a909d263a74aedbbd29da6d5e2c0b6
Evan Cleary <evanjcleary@gmail.com>
2019-11-13 23:33:01 -0500

    makign good progress

--- word.js
+++ word.js
@@ -1,19 +1,39 @@
 var Letter = require("./letter")
 
 
-var Word = function(letterArr) {
-    this.wordString = "",
-    this.letters = letterArr,
-    this.displayWord = function(letter) {
-        
-        var word = ""
-        for (i = 0; i < letterArr.length; i++) {
-            word += letterArr[i]
-            if (i === letterArr.length - 1) {
-            this.wordString = word
-            console.log(this)
+var Word = function (word) {
+    this.wordString = word,
+        this.lettersDisplayed = [],
+        this.lettersArray = [],
+        this.createLetterObjs = function (letter) {
+            this.lettersArray.push(new Letter(letter))
+            this.checkLength(this.wordString)
+        },
+        this.checkLength = function(word) {
+            if (this.lettersArray.length === word.length) {
+                console.log(this)
+            } else {
+                console.log("Still adding letters to: " + word)
             }
+        }, 
+        this.reviewGuess = function(guess) {
+            
+            for ( i = 0; i < this.lettersArray.length; i++ ) {
+                this.lettersArray[i].checkGuess(guess)
+            }
+
+            console.log(this.lettersArray)
         }
-    }
-    
 }
+
+var apples = new Word("apples")
+
+var populateLetters = function (object) {
+    for (i = 0; i < object.wordString.length; i++) {
+        object.createLetterObjs(object.wordString[i])
+    }
+}
+
+populateLetters(apples)
+
+apples.reviewGuess("e")
